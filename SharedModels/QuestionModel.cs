faede43da312161e33d8b42a704a4e63d37ddfdb
Jean-Marc Kraehenbuehl <J.Kraehenbuehl@stud.bbbaden.ch>
2019-04-10 14:45:53 +0200

    add shared models

---
+++ SharedModels/QuestionModel.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace SharedModels
+{
+	class QuestionModel
+	{
+		public int CategoryId { get; set; }
+
+		public string Question { get; set; }
+	}
+}
