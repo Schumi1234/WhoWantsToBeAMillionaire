00b95cdcb4a2aa450ab368cdc73bff325f2cb45b
Jean-Marc Kraehenbuehl <J.Kraehenbuehl@stud.bbbaden.ch>
2019-05-14 13:58:50 +0200

    add new table

---
+++ WebserviceMain/Database/Tables/User.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace WebserviceMain.Database.Tables
+{
+	[Table("tblUser", Schema = "dbo")]
+	public class User
+	{
+		[Key]
+		[DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+		[Display(Name = "User Id")]
+		public int intUserID { get; set; }
+
+		[Required]
+		[Column(TypeName = "nvarchar(max)")]
+		[Display(Name = "Username")]
+		public string strUsername { get; set; }
+
+		[Required]
+		[Column(TypeName = "nvarchar(max)")]
+		//[DataType(DataType.Password)]
+		[Display(Name = "Password")]
+		public string strPassword { get; set; }
+	}
+}
